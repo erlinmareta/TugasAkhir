587cf70dae6a070f3df83a3b44d6dbd27cd65d2a
hafidhsajid <hafidhsajid@gmail.com>
2023-06-15 00:03:24 +0700

    change wrong url asset directory

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -1,63 +1,61 @@
-<script src="../learnly/public/vendor/jquery.min.js"></script>
+<script src="{{ asset('learnly/public/vendor/jquery.min.js') }}"></script>
 
-        <!-- Bootstrap -->
-        <script src="../learnly/public/vendor/popper.min.js"></script>
-        <script src="../learnly/public/vendor/bootstrap.min.js"></script>
+<!-- Bootstrap -->
+<script src="{{ asset('learnly/public/vendor/popper.min.js') }}"></script>
+<script src="{{ asset('learnly/public/vendor/bootstrap.min.js') }}"></script>
 
-        <!-- Perfect Scrollbar -->
-        <script src="../learnly/public/vendor/perfect-scrollbar.min.js"></script>
+<!-- Perfect Scrollbar -->
+<script src="{{ asset('learnly/public/vendor/perfect-scrollbar.min.js') }}"></script>
 
-        <!-- DOM Factory -->
-        <script src="../learnly/public/vendor/dom-factory.js"></script>
+<!-- DOM Factory -->
+<script src="{{ asset('learnly/public/vendor/dom-factory.js') }}"></script>
 
-        <!-- MDK -->
-        <script src="../learnly/public/vendor/material-design-kit.js"></script>
+<!-- MDK -->
+<script src="{{ asset('learnly/public/vendor/material-design-kit.js') }}"></script>
 
-        <!-- App JS -->
-        <script src="../learnly/public/js/app.js"></script>
+<!-- App JS -->
+<script src="{{ asset('learnly/public/js/app.js') }}"></script>
 
-        <!-- Preloader -->
-        <script src="../learnly/public/js/preloader.js"></script>
+<!-- Preloader -->
+<script src="{{ asset('learnly/public/js/preloader.js') }}"></script>
 
 <!-- Global Settings -->
-<script src="../learnly/public/js/settings.js"></script>
+<script src="{{ asset('learnly/public/js/settings.js') }}"></script>
 
 <!-- Flatpickr -->
-<script src="../learnly/public/vendor/flatpickr/flatpickr.min.js"></script>
-<script src="../learnly/public/js/flatpickr.js"></script>
+<script src="{{ asset('learnly/public/vendor/flatpickr/flatpickr.min.js') }}"></script>
+<script src="{{ asset('learnly/public/js/flatpickr.js') }}"></script>
 
 <!-- Moment.js -->
-<script src="../learnly/public/vendor/moment.min.js"></script>
-<script src="../learnly/public/vendor/moment-range.js"></script>
+<script src="{{ asset('learnly/public/vendor/moment.min.js') }}"></script>
+<script src="{{ asset('learnly/public/vendor/moment-range.js') }}"></script>
 
 <!-- Chart.js -->
-<script src="../learnly/public/vendor/Chart.min.js"></script>
-<script src="../learnly/public/js/chartjs.js"></script>
+<script src="{{ asset('learnly/public/vendor/Chart.min.js') }}"></script>
+<script src="{{ asset('learnly/public/js/chartjs.js') }}"></script>
 
 <!-- Chart.js Samples -->
-<script src="../learnly/public/js/page.student-dashboard.js"></script>
+<script src="{{ asset('learnly/public/js/page.student-dashboard.js') }}"></script>
 
 <!-- UI Charts Page JS -->
-<script src="../learnly/public/js/chartjs-rounded-bar.js"></script>
-<script src="../learnly/public/js/chartjs.js"></script>
+<script src="{{ asset('learnly/public/js/chartjs-rounded-bar.js') }}"></script>
+<script src="{{ asset('learnly/public/js/chartjs.js') }}"></script>
 
 <!-- List.js -->
-<script src="../learnly/public/vendor/list.min.js"></script>
-<script src="../learnly/public/js/list.js"></script>
+<script src="{{ asset('learnly/public/vendor/list.min.js') }}"></script>
+<script src="{{ asset('learnly/public/js/list.js') }}"></script>
 
 <!-- Tables -->
-<script src="../learnly/public/js/toggle-check-all.js"></script>
-<script src="../learnly/public/js/check-selected-row.js"></script>
+<script src="{{ asset('learnly/public/js/toggle-check-all.js') }}"></script>
+<script src="{{ asset('learnly/public/js/check-selected-row.js') }}"></script>
 
-  <!-- Quill -->
-<script src="../learnly/public/vendor/quill.min.js"></script>
-<script src="../learnly/public/js/quill.js"></script>
-        
+<!-- Quill -->
+<script src="{{ asset('learnly/public/vendor/quill.min.js') }}"></script>
+<script src="{{ asset('learnly/public/js/quill.js') }}"></script>
+
 <!-- Select2 -->
-<script src="../learnly/public/vendor/select2/select2.min.js"></script>
-<script src="../learnly/public/js/select2.js"></script>
+<script src="{{ asset('learnly/public/vendor/select2/select2.min.js') }}"></script>
+<script src="{{ asset('learnly/public/js/select2.js') }}"></script>
 
- <!-- Highlight.js -->
- <script src="../learnly/public/js/hljs.js"></script>
-
- 
+<!-- Highlight.js -->
+<script src="{{ asset('learnly/public/js/hljs.js') }}"></script>
