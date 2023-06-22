6877160a271919b6b24aeea58ba2fbb23078662a
hafidhsajid <hafidhsajid@gmail.com>
2023-06-22 10:55:22 +0700

    make migrations for catatan table

---
+++ database/migrations/2023_06_18_145214_create_catatan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCatatanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catatan', function (Blueprint $table) {
+            $table->id();
+            $table->integer("kelas_id");
+            $table->integer("user_id");
+            $table->integer("materi_id");
+            $table->string("timestamp");
+            $table->string("catatan");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catatan');
+    }
+}
